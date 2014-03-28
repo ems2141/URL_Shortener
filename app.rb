b8486a47c761366a4504804d0edd8f6459ed2d6e
Ellie Schneiders <ems2141@gmail.com>
2014-03-28 14:16:00 -0600

    [#68381378] User can shorten a URL
    User gets redirected to shortened URL.  Both the old and new URL are visible.
    
    Signed-off-by: Cory Leistikow <cory.leistikow@gmail.com>

--- app.rb
+++ app.rb
@@ -2,8 +2,22 @@ require 'sinatra/base'
 
 class App < Sinatra::Application
 
+  URL_ARRAY = []
+
   get '/' do
     erb :index
+  end
+
+  post '/' do
+    current_index = (URL_ARRAY.length+1).to_s
+    new_url = "http://stormy-inlet-1672.herokuapp.com/" + current_index
+    URL_ARRAY << {:old => params[:url_input], :new => new_url}
+    redirect '/' + current_index + '?stats=true'
+  end
+
+  get '/:id' do
+    url_pairs = URL_ARRAY[params[:id].to_i - 1]
+    erb :show_urls, locals:{:urls => url_pairs}
   end
 
 end
--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -11,6 +11,17 @@ feature "URL Shortener shortens URLs" do
 
     find_field("Enter the URL you would like to 'shorten'").visible?
     find_button('Shorten').visible?
+  end
+
+  scenario "User can shorten URL" do
+
+    visit '/'
+
+    fill_in('url_input', with: "http://tutorials.gschool.it")
+    click_on('Shorten')
+
+    expect(page).to have_content("http://tutorials.gschool.it")
+    expect(page).to have_content("http://stormy-inlet-1672.herokuapp.com/1")
 
   end
 end
