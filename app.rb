f4eda57e90927d2128a88bc06c0a1d757b5d1459
Ellie Schneiders <ems2141@gmail.com>
2014-04-01 14:35:07 -0600

    [#68381384] error message disappears when user refreshes browser
    Signed-off-by: Jenny Allar <jennyallar@gmail.com>

--- app.rb
+++ app.rb
@@ -1,12 +1,14 @@
 require 'sinatra/base'
 
+
 class App < Sinatra::Application
 
   URL_ARRAY = []
-  URL_ERROR = false
+  URL_ERROR = {}
 
   get '/' do
-    if URL_ERROR
+    if URL_ERROR[:status] == true
+      URL_ERROR[:status] = false
       erb :index, locals: {:error => URL_ERROR[:error_text], :url_value => URL_ERROR[:bad_url]}
     else
       erb :index, locals: {:error => nil, :url_value => ""}
@@ -14,18 +16,17 @@ class App < Sinatra::Application
   end
 
   post '/' do
-    if params[:url_input] =~ URI::regexp(["ftp", "http", "https"])
-      URL_ERROR = false
+    if params[:url_input] =~ /^(http|https):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(([0-9]{1,5})?\/.*)?$/ix
+      URL_ERROR[:status] = false
       current_index = (URL_ARRAY.length + 1).to_s
       @base_url ||= "#{request.env['rack.url_scheme']}://#{request.env['HTTP_HOST']}"
       new_url = @base_url + '/' + current_index
       URL_ARRAY << {:old => params[:url_input], :new => new_url}
       redirect '/' + current_index + '?stats=true'
     else
-      URL_ERROR = {error_text: "The text you entered is not a valid URL", bad_url: params[:url_input]}
+      URL_ERROR = {error_text: "The text you entered is not a valid URL", bad_url: params[:url_input], status: true}
       redirect '/'
     end
-
   end
 
   get '/:id' do
@@ -37,6 +38,8 @@ class App < Sinatra::Application
     end
   end
 
-  #"http://google.com" =~ URI::regexp(["ftp", "http", "https"])
+  get '/favicon.ico' do
+    #NOOP
+  end
 
 end
